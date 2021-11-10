541f27508f7829cdfc6d61188f635b1d3a2bf8a9
Jesus Arath <jesus.flores.p19@gmail.com>
2021-11-10 16:10:37 -0600

    Agregamos el modulo pages al app.modulle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { CarteleraResponse } from './interface/cartelera.interface';
-import { PeliculasService } from './services/peliculas.service';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +6,5 @@ import { PeliculasService } from './services/peliculas.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'moviesApp';
-
-  constructor(public peliculasService: PeliculasService){
-    this.peliculasService.getCartelera().subscribe((data) => {
-      console.log(data);
-    })
-  }
+  title = 'PeliculasApp';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { ComponentsModule } from './components/components.module';
     BrowserModule,
     AppRoutingModule,
     ComponentsModule,
+    PagesModule,
 
     HttpClientModule
   ],
