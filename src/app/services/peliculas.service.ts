a3d2fdf9faa0a7caa87eaa391fb218351e20ddd7
Jesus Arath <jesus.flores.p19@gmail.com>
2021-11-10 14:08:09 -0600

    getCartelera con tipado

--- src/app/services/peliculas.service.ts
+++ src/app/services/peliculas.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http"
+import { Observable } from 'rxjs';
+import { CarteleraResponse } from '../interface/cartelera.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PeliculasService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getCartelera(): Observable<CarteleraResponse>{
+    return this.http.get<CarteleraResponse>("https://api.themoviedb.org/3/movie/now_playing?api_key=2819c6599132cb1c015ba87a44ea1cb5&language=en-US&page=1");
+  }
 }
