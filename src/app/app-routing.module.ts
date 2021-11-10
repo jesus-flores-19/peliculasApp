01d6809753d340df0655c2d55cdf6202bef0435a
Jesus Arath <jesus.flores.p19@gmail.com>
2021-11-10 14:47:25 -0600

    Creamos rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BuscarCComponent } from './pages/buscar-c/buscar-c.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PeliculaComponent } from './pages/pelicula/pelicula.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"home", component: HomeComponent
+  },
+  {
+    path: "pelicula/:id", component: PeliculaComponent
+  },
+  {
+    path: "buscar/:texto", component: BuscarCComponent
+  },{
+    path: "**", redirectTo:"home"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
