38a56c641d409b1bc185aba2cefc4bc7b4a917db
Jesus Arath <jesus.flores.p19@gmail.com>
2021-11-10 14:07:58 -0600

    Agregamos data de componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CarteleraResponse } from './interface/cartelera.interface';
+import { PeliculasService } from './services/peliculas.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'moviesApp';
+
+  constructor(public peliculasService: PeliculasService){
+    this.peliculasService.getCartelera().subscribe((data) => {
+      console.log(data);
+    })
+  }
 }
