f486a83d619f97615b8046a62e3316697f3b3d18
Jesus Arath <jesus.flores.p19@gmail.com>
2021-11-09 22:22:41 -0600

    Componente buscar

---
+++ src/app/pages/buscar-c/buscar-c.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-buscar-c',
+  templateUrl: './buscar-c.component.html',
+  styleUrls: ['./buscar-c.component.css']
+})
+export class BuscarCComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
