5b82b2d98020abdc6a9f66f9a738cca7ea2ab9ba
Jesus Arath <jesus.flores.p19@gmail.com>
2021-11-10 16:09:16 -0600

    exportamos el componente slideshow

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule } from '@angular/router';
+
+import { NavbarComponent } from './navbar/navbar.component';
+import { SlideshowComponent } from './slideshow/slideshow.component';
 
 
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    SlideshowComponent
   ],
   exports: [
-    NavbarComponent
+    NavbarComponent,
+    SlideshowComponent
   ],
   imports: [
     CommonModule,
